77f5337b7722c5afc24ac5e20a9c345705e5b8d8
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 15:09:48 +0300

    Modal.css file is formed and imported by About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,25 @@
 import React from "react";
+import "./Modal.css";
 
 const About = () => {
   return (
-    <div
-      className="ui raised very padded text comtainer segment"
-      style={{ marginTop: "80px" }}
-    >
+    <div>
+      <div className="modal">
+          <div className="modal-content">
+              <h3>I am a modal</h3>
+          </div>
+      </div>
+
+      <div
+        className="ui raised very padded text comtainer segment"
+        style={{ marginTop: "80px" }}
+      >
         <h3 className="ui hader">About</h3>
-        <p>Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet.</p>
+        <p>
+          Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu
+          amet.
+        </p>
+      </div>
     </div>
   );
 };
