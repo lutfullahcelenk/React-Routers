707c919afd770b6d22f53da68e08f4127368ec6f
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 09:31:03 +0300

    Navbar section is formed and pages are loaded.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
     return (
         <nav className="ui raised very padded segment">
             <a className="ui teal inverted segment">Gloria</a>
             <div className="ui right floated header">
-                <button className="ui button">Home</button>
-                <button className="ui button">About</button>
-                <button className="ui button">Contact</button>
+                <button className="ui button"> <a href="/"> Home</a></button>
+                <button className="ui button"> <a href="/about"> About</a></button>
+                <button className="ui button"> <a href="/contact"> Contact</a></button>
             </div>
         </nav>
     )
