c0d3735023e8b8ca330f9e389c51dfeaa1bc6538
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 09:24:05 +0300

    About.js file ais added and formed as functional component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      className="ui raised very padded text comtainer segment"
+      style={{ marginTop: "80px" }}
+    >
+        <h3 className="ui hader">About</h3>
+        <p>Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet.</p>
+    </div>
+  );
+};
+
+export default About;
