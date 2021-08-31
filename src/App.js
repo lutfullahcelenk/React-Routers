a5e41a7392cf7814769198e73df520aa9b73b051
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 09:50:19 +0300

    Link to attribute is imported and used in Navbar section to block refreshing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="ui raised very padded segment">
             <a className="ui teal inverted segment">Gloria</a>
             <div className="ui right floated header">
-                <button className="ui button"> <a href="/"> Home</a></button>
-                <button className="ui button"> <a href="/about"> About</a></button>
-                <button className="ui button"> <a href="/contact"> Contact</a></button>
+                <button className="ui button"> <Link to="/"> Home</Link></button>
+                <button className="ui button"> <Link to="/about"> About</Link></button>
+                <button className="ui button"> <Link to="/contact"> Contact</Link></button>
             </div>
         </nav>
     )
