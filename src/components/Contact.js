1384817269cdc0fe84889bef14705428abfa133d
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 09:24:39 +0300

    Contact.js file is added and formed as functional component

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      className="ui raised very padded text comtainer segment"
+      style={{ marginTop: "80px" }}
+    >
+        <h3 className="ui hader">Contact</h3>
+        <p>Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet.</p>
+    </div>
+  );
+};
+
+export default Contact;
