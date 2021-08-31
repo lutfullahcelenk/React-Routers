efa95580e83dac0a2315f8d72271067da85f54b8
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 09:23:22 +0300

    Home.js file is formed and export to App file

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      className="ui raised very padded text comtainer segment"
+      style={{ marginTop: "80px" }}
+    >
+        <h3 className="ui hader">Home</h3>
+        <p>Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet.</p>
+    </div>
+  );
+};
+
+export default Home;
