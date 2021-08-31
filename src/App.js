abac9583fa2151b5f799a497e755fba4054bb53e
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-31 10:02:08 +0300

    noon break

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,7 +13,6 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
-
       </div>
     </Router>
   );
