b8dfb34d9ef55ff5ee92afc73310a5b518c9d59a
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-01 10:18:22 +0300

    createStore is pulled from redux library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from "redux";
+
+const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
