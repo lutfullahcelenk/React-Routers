68b792f8c15703a7ac4fa1dba4bd1185aba588eb
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-01 10:28:10 +0300

    rootReducer is imported to index and used in parameters for store to combine components and index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
