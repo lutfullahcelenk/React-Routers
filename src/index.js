44a00dbc8d48abba3a6d2d4834855e58835ca283
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-01 10:19:42 +0300

    Provider is pulled from react-redux library to combine components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 const store = createStore();
 
