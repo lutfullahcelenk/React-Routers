d38d472036aa250174b344ccdba4611a5fe94bde
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-02 08:31:46 +0300

    three objects are added to initilaState

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const initialState = {
-    cards: []
+    cards: [
+        {id : "1" ,title: "Alex",  body: "Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet."},
+        {id : "2" ,title: "Willma",  body: "Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet."},
+        {id : "3" ,title: "Lütfullah",  body: "Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu amet."}
+    ]
 }
 
 const rootReducer = (state=initialState,action) => {
