2f372b3b4d931c26b1a23a5787b5ba51097f1a60
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-02 08:53:24 +0300

    we modified the component with state's props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,38 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import Modal from './Modal';
-import {connect} from 'react-redux';
+import { Link } from "react-router-dom";
+// import Modal from './Modal';
+import { connect } from "react-redux";
 
-const Contact = (props) => {
-  // console.log(props)
+const Contact = ({ cards }) => {
+  // console.log(props.cards)
 
   return (
     <div>
-      <Modal />
-      <div
-        className="ui raised very padded text comtainer segment"
-        style={{ marginTop: "80px" }}
-      >
-        <Link to="/alex">Alex</Link>
-        <p>
-          Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu
-          amet.
-        </p>
-      </div>
-      <div
-        className="ui raised very padded text comtainer segment"
-        style={{ marginTop: "80px" }}
-      >
-        <Link to="/lutfullah">Lutfullah</Link>
-        <p>
-          Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu
-          amet.
-        </p>
-      </div>
+      {/* <Modal /> */}
+
+      {cards.map((card) => {
+        return (
+          <div
+            className="ui raised very padded text comtainer segment"
+            style={{ marginTop: "80px" }}
+            key = {card.id}
+          >
+            <Link to={`/${card.title}`}>{card.title}</Link>
+            <p>
+              {card.body}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default connect(Contact) ;
+const mapStateToProps = (state) => {
+  const { cards } = state;
+  return {
+    cards: cards,
+  };
+};
+
+export default connect(mapStateToProps)(Contact);
