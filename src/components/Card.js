6ff4a651cfe4936dbf042146d92bd8cf5177846d
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-02 09:32:27 +0300

    connect is implemented and props sent to Card component to be used in div

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,27 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 // import {useParams} from 'react-router';
 import { connect } from "react-redux";
 
 
-const Card = ({match}) => {
-
-    const [userName,setUserName] = useState("")
-
-    useEffect(() => {
-        setUserName(match.params.profile)
-    })
+const Card = ({card}) => {
 
   return (
     <div
       className="ui raised very padded text comtainer segment"
       style={{ marginTop: "80px" }}
     >
-      <h3 className="ui hader">{userName}</h3>
+      <h3 className="ui header">{card.title}</h3>
+      <p>{card.body}</p>
     </div>
   );
 };
 
-export default connect(Card);
+
+const mapStateToProps = (state,ownProps) => {
+  let title = ownProps.match.params.profile;
+  return {
+    card : state.cards.find(card => card.title === title)
+  }
+};
+
+export default connect(mapStateToProps)(Card);
